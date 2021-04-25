3545d0e48fe1bc926e1d07ce64140625e2b54019
yangkerrr <yangkerrr@163.com>
2021-04-25 20:40:32 +0800

    4.25

--- qt/layout.py
+++ qt/layout.py
@@ -2,13 +2,11 @@
 
 # Form implementation generated from reading ui file 'layout.ui'
 #
-# Created by: PyQt5 UI code generator 5.14.1
+# Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost!
 
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -85,23 +83,24 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "手写数字识别GUI-v1.0 --by hamlin"))
-        self.cbBox_Mode.setItemText(0, _translate("MainWindow", "1：MINIST随机抽取"))
+        self.cbBox_Mode.setItemText(0, _translate("MainWindow", "1：测试集随机抽取"))
         self.cbBox_Mode.setItemText(1, _translate("MainWindow", "2：鼠标手写输入"))
         self.label.setText(_translate("MainWindow", "模式选择"))
         self.pbtClear.setText(_translate("MainWindow", "清除数据"))
-        self.pbtGetMnist.setText(_translate("MainWindow", "MNIST抽取"))
+        self.pbtGetMnist.setText(_translate("MainWindow", "测试集抽取"))
         self.pbtPredict.setText(_translate("MainWindow", "识别"))
         self.label_3.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:12pt; font-weight:600;\">识别结果：</span></p><p><br/></p><p><br/></p><p><span style=\" font-size:12pt; font-weight:600;\">Softmax：</span></p></body></html>"))
         self.label_4.setText(_translate("MainWindow", "数据输入区域"))
         self.label_5.setText(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu\'; font-weight:600;\">使用说明</span></p>\n"
-"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu\';\">1、点击下拉列表进行模式选择，输入待识别数据后点击“识别”按键进行识别</span></p>\n"
-"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu\';\">2、经CNN网络计算后输出，显示识别结果与Softmax值</span></p>\n"
-"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu\';\">3、点击“清除数据”按键重新输入数据</span></p>\n"
-"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu\';\">模式1：随机从测试集抽取图像作为待识别数据，点击“MNIST抽取”按键抽取</span></p>\n"
-"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu\';\">模式2：使用鼠标在数据输入区域手写输入作为待识别数据</span></p></body></html>"))
+"</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:600;\">使用说明</span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu\'; font-size:11pt;\">1、点击下拉列表进行模式选择，输入待识别数据后点击“识别”按键进行识别</span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu\'; font-size:11pt;\">2、经LeNet网络计算后输出，显示识别结果与Softmax值</span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu\'; font-size:11pt;\">3、点击“清除数据”按键重新输入数据</span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu\'; font-size:11pt;\">模式1：随机从测试集抽取图像作为待识别数据，点击“测试集抽取”按键抽取</span></p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Ubuntu\'; font-size:11pt;\">模式2：使用鼠标在数据输入区域手写输入作为待识别数据</span></p></body></html>"))
         self.lbResult.setText(_translate("MainWindow", "9"))
         self.lbCofidence.setText(_translate("MainWindow", "0.99999999"))
+
